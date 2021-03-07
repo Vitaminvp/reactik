a4cd169aaac1f290d30751e6e08d4b178189d27c
Vitamin <vitamin@ukr.net>
2021-03-07 22:38:22 +0200

    add reconciliation

--- src/index.js
+++ src/index.js
@@ -3,24 +3,24 @@ import Reactik from "./reactic";
 /** @jsx Reactik.createElement */
 const container = document.getElementById("root");
 
-const updateValue = ({ target: { value } }) => {
-  App(value);
-};
-
-const App = value => {
+const App = (text, value) => {
   const element = (
     <div>
-      <h2>My favourite language is {value}</h2>
+      <h2>My favourite language is {text}</h2>
       <div>
         <label htmlFor="languages">Choose your favourite language: </label>
-        <select onChange={updateValue} name="languages" id="languages">
-          <option value="Java Script" selected={"Java Script" === value}>
+        <select
+          onChange={({ target: { value: newText } }) => App(newText, value)}
+          name="languages"
+          id="languages"
+        >
+          <option value="Java Script" selected={"Java Script" === text}>
             Java Script
           </option>
-          <option value="Coffee Script" selected={"Coffee Script" === value}>
+          <option value="Coffee Script" selected={"Coffee Script" === text}>
             Coffee Script
           </option>
-          <option value="Type Script" selected={"Type Script" === value}>
+          <option value="Type Script" selected={"Type Script" === text}>
             Type Script
           </option>
         </select>
@@ -34,6 +34,13 @@ const App = value => {
           disabled={true}
         />
         <label htmlFor="joke">I'm joking</label>
+      </div>
+      <div>
+        <input
+          onInput={({ target: { value: newValue } }) => App(text, newValue)}
+          value={value}
+        />
+        <h2>Hello {value}</h2>
       </div>
     </div>
   );
--- src/reactic/index.js
+++ src/reactic/index.js
@@ -1,5 +1,7 @@
 let nextUnitOfWork = null;
 let wipRoot = null;
+let currentRoot = null;
+let deletions = null;
 
 const isObject = value => value === Object(value);
 
@@ -21,27 +23,93 @@ const createElement = (type, props, ...children) => ({
   }
 });
 
-const isEvent = key => key.startsWith("on");
-const isProperty = key => key !== "children";
-
 function createDom(fiber) {
   const dom =
     fiber.type === "TEXT_ELEMENT"
       ? document.createTextNode("")
       : document.createElement(fiber.type);
 
-  Object.keys(fiber.props)
-    .filter(isProperty)
-    .forEach(name => {
-      if (isEvent(name)) {
-        const eventType = name.toLowerCase().substring(2);
-        dom.addEventListener(eventType, fiber.props[name]);
-      } else {
-        dom[name] = fiber.props[name];
-      }
-    });
+  updateDom(dom, {}, fiber.props);
 
   return dom;
+}
+
+const isEvent = key => key.startsWith("on");
+const isProperty = key => key !== "children" && !isEvent(key);
+const isNew = (prev, next) => key => prev[key] !== next[key];
+const isGone = (prev, next) => key => !(key in next);
+
+function updateDom(dom, prevProps, nextProps) {
+  //Remove old or changed event listeners
+  Object.keys(prevProps)
+    .filter(isEvent)
+    .filter(key => !(key in nextProps) || isNew(prevProps, nextProps)(key))
+    .forEach(name => {
+      const eventType = name.toLowerCase().substring(2);
+      dom.removeEventListener(eventType, prevProps[name]);
+    });
+
+  // Remove old properties
+  Object.keys(prevProps)
+    .filter(isProperty)
+    .filter(isGone(prevProps, nextProps))
+    .forEach(name => {
+      dom[name] = "";
+    });
+
+  // Set new or changed properties
+  Object.keys(nextProps)
+    .filter(isProperty)
+    .filter(isNew(prevProps, nextProps))
+    .forEach(name => {
+      dom[name] = nextProps[name];
+    });
+
+  // Add event listeners
+  Object.keys(nextProps)
+    .filter(isEvent)
+    .filter(isNew(prevProps, nextProps))
+    .forEach(name => {
+      const eventType = name.toLowerCase().substring(2);
+      dom.addEventListener(eventType, nextProps[name]);
+    });
+}
+
+function commitRoot() {
+  deletions.forEach(commitWork);
+  commitWork(wipRoot.child);
+  currentRoot = wipRoot;
+  wipRoot = null;
+}
+
+function commitWork(fiber) {
+  if (!fiber) return;
+
+  const domParent = fiber.parent.dom;
+
+  if (fiber.effectTag === "PLACEMENT" && fiber.dom != null) {
+    domParent.appendChild(fiber.dom);
+  } else if (fiber.effectTag === "UPDATE" && fiber.dom != null) {
+    updateDom(fiber.dom, fiber.alternate.props, fiber.props);
+  } else if (fiber.effectTag === "DELETION") {
+    domParent.removeChild(fiber.dom);
+  }
+
+  commitWork(fiber.child);
+  commitWork(fiber.sibling);
+}
+
+function render(element, container) {
+  // container.innerHTML = null;
+  wipRoot = {
+    dom: container,
+    props: {
+      children: [element]
+    },
+    alternate: currentRoot
+  };
+  nextUnitOfWork = wipRoot;
+  deletions = [];
 }
 
 function workLoop(deadline) {
@@ -56,48 +124,16 @@ function workLoop(deadline) {
   }
   requestIdleCallback(workLoop);
 }
-
-function commitRoot() {
-  commitWork(wipRoot.child);
-  wipRoot = null;
-}
-
-function commitWork(fiber) {
-  if (!fiber) return;
-
-  const domParent = fiber.parent.dom;
-  domParent.appendChild(fiber.dom);
-  commitWork(fiber.child);
-  commitWork(fiber.sibling);
-}
+requestIdleCallback(workLoop);
 
 function performUnitOfWork(fiber) {
   if (!fiber.dom) {
     fiber.dom = createDom(fiber);
   }
 
-  if (fiber.parent) {
-    fiber.parent.dom.appendChild(fiber.dom);
-  }
+  const elements = fiber.props.children;
 
-  let prevSibling = null;
-
-  fiber.props.children.forEach(({ type, props }, index) => {
-    const newFiber = {
-      type,
-      props,
-      parent: fiber,
-      dom: null
-    };
-
-    if (index === 0) {
-      fiber.child = newFiber;
-    } else {
-      prevSibling.sibling = newFiber;
-    }
-
-    prevSibling = newFiber;
-  });
+  reconcileChildren(fiber, elements);
 
   if (fiber.child) {
     return fiber.child;
@@ -111,17 +147,55 @@ function performUnitOfWork(fiber) {
   }
 }
 
-function render(element, container) {
-  container.innerHTML = null;
-  wipRoot = {
-    dom: container,
-    props: {
-      children: [element]
-    }
-  };
-  nextUnitOfWork = wipRoot;
+function reconcileChildren(wipFiber, elements) {
+  let index = 0;
+  let oldFiber = wipFiber.alternate && wipFiber.alternate.child;
+  let prevSibling = null;
 
-  requestIdleCallback(workLoop);
+  while (index < elements.length || oldFiber != null) {
+    const element = elements[index];
+    let newFiber = null;
+
+    const sameType = oldFiber && element && element.type === oldFiber.type;
+
+    if (sameType) {
+      newFiber = {
+        type: oldFiber.type,
+        props: element.props,
+        dom: oldFiber.dom,
+        parent: wipFiber,
+        alternate: oldFiber,
+        effectTag: "UPDATE"
+      };
+    }
+    if (element && !sameType) {
+      newFiber = {
+        type: element.type,
+        props: element.props,
+        dom: null,
+        parent: wipFiber,
+        alternate: null,
+        effectTag: "PLACEMENT"
+      };
+    }
+    if (oldFiber && !sameType) {
+      oldFiber.effectTag = "DELETION";
+      deletions.push(oldFiber);
+    }
+
+    if (oldFiber) {
+      oldFiber = oldFiber.sibling;
+    }
+
+    if (index === 0) {
+      wipFiber.child = newFiber;
+    } else if (element) {
+      prevSibling.sibling = newFiber;
+    }
+
+    prevSibling = newFiber;
+    index++;
+  }
 }
 
 const Reactik = {
